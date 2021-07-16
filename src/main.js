871acc1bab77300ef7140bc86c791b5e184ee239
alexalspaugh <alexalspaugh@me.com>
2021-07-15 23:34:39 -0400

    added objects to pages array

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ const prevTag = document.querySelector('.prev')
 const outputTag = document.querySelector('h2')
 
 const pages = [
-    {copy = "a Brooklyn-based graphic designer"},
-    {copy = "Kanye West's biggest fan"},
-    {copy = "looking for a job at the start of October"},
-    {copy = "letting you <a href="pdf.pdf">download her PDF</a>"}
+    {copy: "a Brooklyn-based graphic designer"},
+    {copy: "Kanye West's biggest fan"},
+    {copy: "looking for a job at the start of October"},
+    {copy: 'letting you <a href="pdf.pdf">download her PDF</a>'}
 ]
 
 let pageNumber = 0
@@ -32,7 +32,7 @@ const prev = function () {
 }
 
 const update = function () {
-    outputTag.innerHTML = pages[pageNumber]
+    outputTag.innerHTML = pages[pageNumber].copy
 }
 
 nextTag.addEventListener('click', function() {
