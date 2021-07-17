3cef67185fe093cff60cc2728269381de82ef7cd
alexalspaugh <alexalspaugh@me.com>
2021-07-17 10:21:21 -0400

    added in array objects and random function

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 const nextTag = document.querySelector('.next')
 const prevTag = document.querySelector('.prev')
 const outputTag = document.querySelector('h2')
+const circleTag = document.querySelector('.circle')
+const bodyTag = document.querySelector('body')
+const randomTag = document.querySelector('.random')
 
 const pages = [
-    {copy: "a Brooklyn-based graphic designer"},
-    {copy: "Kanye West's biggest fan"},
-    {copy: "looking for a job at the start of October"},
-    {copy: 'letting you <a href="pdf.pdf">download her PDF</a>'}
+    { copy: "a Brooklyn-based graphic designer", background: "#edc7a9", circle: "#3e78ed" },
+    { copy: "Kanye West's biggest fan", background: "#a1fffe", circle: "#e34a47" },
+    { copy: "looking for a job at the start of October", background: "#d3c7f3", circle: "#f7fe00" },
+    { copy: 'letting you <a href="pdf.pdf">download her PDF</a>', background: "#faffb8", circle: "#b472e6" }
 ]
 
 let pageNumber = 0
+
+const random = function () {
+    pageNumber = Math.floor( Math.random() * pages.length)
+
+    update()
+}
 
 const next = function () {
     pageNumber = pageNumber + 1
@@ -33,6 +42,8 @@ const prev = function () {
 
 const update = function () {
     outputTag.innerHTML = pages[pageNumber].copy
+    bodyTag.style.backgroundColor = pages[pageNumber].background
+    circleTag.style.backgroundColor = pages[pageNumber].circle
 }
 
 nextTag.addEventListener('click', function() {
@@ -42,3 +53,9 @@ nextTag.addEventListener('click', function() {
 prevTag.addEventListener('click', function () {
     prev()
 })
+
+randomTag.addEventListener('click', function () {
+    random()
+})
+
+
